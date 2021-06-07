a5bfa80dd5500a9a3d572ce30c5d5d07859c6b9f
FrancescoBiasiIIOT <francesco.biasi@stud.tecnicosuperiorekennedy.it>
2021-06-07 17:09:20 +0200

    Aggiunto test device

---
+++ ITS.PWIIOT.SmartClassrooms/ITS.PWIIOT.SmartClassrooms.ApplicationCore/IOT-Hub services/DeviceService.cs
@@ -0,0 +1,38 @@
+﻿using ITS.PWIIOT.SmartClassrooms.ApplicationCore.Interfaces;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITS.PWIIOT.SmartClassrooms.ApplicationCore.IOT_Hub_services
+{
+    public class DeviceService : IDeviceService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _iotHubConnectionString;
+        private DeviceClient _deviceClient; //device client non può essere settato nel costruttore perchè dipende dal dispositivo che chiama
+        public DeviceService(IConfiguration configuration)
+        {
+            _iotHubConnectionString = _configuration.GetConnectionString("IotHub");
+        }
+        public async Task<string> ReceiveMessageFromHub(string deviceId)
+        {
+            TimeSpan s_receiveTimeout = TimeSpan.FromSeconds(1);
+            var deviceConnectionString = "HostName=ProjectWorkHub.azure-devices.net;DeviceId=RB100;SharedAccessKey=D5b4w4vff+hf5ocafWsJ8IDtxGdVlSnlZpBPFnHDyMc=";
+            var deviceClient = DeviceClient.CreateFromConnectionString(
+                   deviceConnectionString,
+                   Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+            var receivedMessage = await deviceClient.ReceiveAsync();
+            if (receivedMessage != null)
+            {
+                var message = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return message;
+            }
+            else return null;
+        }
+    }
+}
---
+++ ITS.PWIIOT.SmartClassrooms/ITS.PWIIOT.SmartClassrooms.TestWorker/Worker.cs
@@ -0,0 +1,35 @@
+using ITS.PWIIOT.SmartClassrooms.ApplicationCore.Interfaces;
+using ITS.PWIIOT.SmartClassrooms.Domain;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ITS.PWIIOT.SmartClassrooms.TestWorker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IDeviceService _deviceService;
+        public Worker(ILogger<Worker> logger, IDeviceService deviceService)
+        {
+            _logger = logger;
+            _deviceService = deviceService;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var message = await _deviceService.ReceiveMessageFromHub("RB100");
+                Lesson lesson = JsonSerializer.Deserialize<Lesson>(message);
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
