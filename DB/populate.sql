058f9826c5835a33bdf214e5f7d6f68b04441c5b
Valerio Anzilotti <valerio.anzilotti.nitto@tecnicosuperiorekennedy.it>
2021-06-07 15:17:38 +0200

    Riempito il database

--- DB/populate.sql
+++ DB/populate.sql
@@ -330,4 +330,129 @@ INSERT INTO [dbo].[Gateways]
 GO
 
 
+INSERT INTO [dbo].[Subjects]
+           ([Id]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ('4597d46e-5abf-494e-96cc-6f20110b43e2'
+           ,'Sistemi',
+		   '')
+GO
+
+
+INSERT INTO [dbo].[Subjects]
+           ([Id]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ('5a096e9a-5dd1-4b34-84c4-f2ed66c32941'
+           ,'Soc e single board',
+		   '')
+GO
+
+INSERT INTO [dbo].[Subjects]
+           ([Id]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ('d263a1bd-1d19-4f38-9186-779d2cbc0444'
+           ,'Diritto aziendale',
+		   '')
+GO
+
+INSERT INTO [dbo].[Subjects]
+           ([Id]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ('6aefe790-c5c7-461b-8cf5-e8a0596f4560'
+           ,'Elettronica',
+		   '')
+GO
+
+INSERT INTO [dbo].[Subjects]
+           ([Id]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ('85490d40-dd73-4c14-8531-e23de51cd6a2'
+           ,'Programmazione C#',
+		   '')
+GO
+
+INSERT INTO [dbo].[Subjects]
+           ([Id]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ('6787ac77-b407-4d02-9fd5-d0a0c7d9b1de'
+           ,'sistemi embedded',
+		   '')
+GO
+
+
+INSERT INTO [dbo].[Microcontrollers]
+           ([Id]
+           ,[DeviceId]
+           ,[ClassroomId]
+           ,[GatewayId])
+     VALUES
+           ('399f1f33-c6dc-47ae-9ab6-ac90123c9f60'
+           ,'PIC01'
+           ,'797842B3-6280-4EFA-B04D-11075CB75A2B'
+           ,'49F3B0F1-0550-4148-B604-05F2BBB40AB6')
+GO
+
+
+INSERT INTO [dbo].[Microcontrollers]
+           ([Id]
+           ,[DeviceId]
+           ,[ClassroomId]
+           ,[GatewayId])
+     VALUES
+           ('70fa78fc-6fc4-406e-9204-32c9b73a058e'
+           ,'PIC02'
+           ,'3E7F0DAA-1E21-45C5-88D5-17E4BB59E481'
+           ,'D6496A60-3DFB-438D-9146-069B1CC0201A')
+GO
+
+
+INSERT INTO [dbo].[Microcontrollers]
+           ([Id]
+           ,[DeviceId]
+           ,[ClassroomId]
+           ,[GatewayId])
+     VALUES
+           ('7b58e547-2cd0-4397-a4ec-f12e61257631'
+           ,'PIC03'
+           ,'303FB111-8FC1-4E40-A040-3A43A09A7326'
+           ,'6728E870-6557-46FB-A9ED-29FAECD92E38')
+GO
+
+
+INSERT INTO [dbo].[Microcontrollers]
+           ([Id]
+           ,[DeviceId]
+           ,[ClassroomId]
+           ,[GatewayId])
+     VALUES
+           ('eaca1607-1543-4582-99d0-c0247736b5dd'
+           ,'PIC04'
+           ,'CA14F180-881B-43F6-8CBE-80B4F8F61AAC'
+           ,'6E48857C-3A40-468F-BC7B-2E0B27CFF0A5')
+GO
+
+
+INSERT INTO [dbo].[Microcontrollers]
+           ([Id]
+           ,[DeviceId]
+           ,[ClassroomId]
+           ,[GatewayId])
+     VALUES
+           ('1fcf0837-44cb-4483-9234-cb0926f5acf8'
+           ,'PIC05'
+           ,'8B1B33DC-60B6-4480-A113-81FD30479E60'
+           ,'35276DD7-4929-409D-8C11-D1F2C3C0A8D6')
+GO
 
