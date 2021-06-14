c67d8c8226cd69d7b4432b09378bfa38b58300a4
FrancescoBiasiIIOT <francesco.biasi@stud.tecnicosuperiorekennedy.it>
2021-06-14 15:59:05 +0200

    Aggiunto: receiver worker

--- ITS.PWIIOT.SmartClassrooms/ITS.PWIIOT.SmartClassrooms.WebApplication/Services/ReceiverWorker.cs
+++ ITS.PWIIOT.SmartClassrooms/ITS.PWIIOT.SmartClassrooms.WebApplication/Services/ReceiverWorker.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using ITS.PWIIOT.SmartClassrooms.ApplicationCore.Interfaces;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,22 @@ namespace ITS.PWIIOT.SmartClassrooms.WebApplication.Services
     public class ReceiverWorker : BackgroundService
     {
         //Worker per ricevere i messaggi dal dispositivo 
-        public ReceiverWorker()
+        private readonly IMessage _message;
+        private readonly IClassroomService _classroomService;
+        public ReceiverWorker(IMessage message, IClassroomService classroomService)
         {
+            _message = message;
+            _classroomService = classroomService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-
+            await _message.StartReceiveMessagesFromSubscriptionAsync(
+          message =>
+          { 
+                _classroomService.SetClassroomAvailable("");
+              //NOTIFICARE A TUTTI I CLIENT CHE L'AULA è LIBERA
+          }, "ITS/classroom/available");
         }
     }
 }
