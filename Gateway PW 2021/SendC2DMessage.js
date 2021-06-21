98fb594337bf09b4085d1777b2cc49884a659f93
FrancescoBiasiIIOT <francesco.biasi@stud.tecnicosuperiorekennedy.it>
2021-06-21 15:50:40 +0200

    aggiunta

--- Gateway PW 2021/SendC2DMessage.js
+++ Gateway PW 2021/SendC2DMessage.js
@@ -3,7 +3,8 @@
 var fs = require('fs');
 var Mqtt = require('azure-iot-device-mqtt').Mqtt;
 var DeviceClient = require('azure-iot-device').Client
-
+const schedule = require('node-schedule');
+const date = new Date(2021,6,21,14,58,0);
 var connectionString = 'HostName=ProjectWorkHub.azure-devices.net;DeviceId=TestDato;SharedAccessKey=NbF6zU6Vbjf9G8PRuvp8DwQRO8gFTh27L19KuHrW7/k=';
 var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
 
@@ -45,6 +46,7 @@ function ManageMessage(msg)
         var data = JSON.parse(msg);
         console.log(data);
         if(err) throw err;
+        ScheduleLessons(data);
       })
     break;
 //-----------------------------
@@ -59,4 +61,26 @@ function ManageMessage(msg)
   }
 }
 
+ScheduleLessons()
+function ScheduleLessons()
+{
+    var date =  new Date(2021,5,21,15,50,0);
+    var obj = JSON.parse(fs.readFileSync('calendar.json', 'utf8'));
+    var classrooms = obj.ClassroomsCalendar.map(c => c.ClassroomId);
+   
+    classrooms.forEach(classroom => {
+        var classroomsLessons = obj.ClassroomsCalendar.find(c => c.ClassroomId == classroom).Lessons;
+        var nextLesson = classroomsLessons.find(c => new Date(c.StartDate) > date);
+        var dateTest = new Date(nextLesson.StartDate);
+        var month = dateTest.getMonth();
+        var day  = dateTest.getDay();
+        var hour = dateTest.getHours();
+        console.log("mese" + month)
+        console.log("giorno"+ day )
+        console.log("ora"+  hour)
+    });
+    const job = schedule.scheduleJob(date, ()=>{
+      console.log("ciao");
+    })
 
+}
